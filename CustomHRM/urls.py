2c3ba333cf821fe60b9c884b8061e936d47f43fe
Tory Martin <tory.martin@gmail.com>
2015-12-05 14:55:31 -0500

    Updated Static paths for server
    added admin autodiscover to urls
    wsgi change to file

--- CustomHRM/urls.py
+++ CustomHRM/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^', include('problems.urls')),
