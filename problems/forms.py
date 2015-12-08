95d188f026589e0b802c0c53d85a30e893901c6b
ocoss <otcoss@gmail.com>
2015-12-08 15:10:29 -0500

    fixed initial memory bug

--- problems/forms.py
+++ problems/forms.py
@@ -22,7 +22,11 @@ class CodeForm(forms.Form):
         LETTERS = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
                     'p','q','r','s','t','u','v','w','x','y','z']
         LBL_PTRN = re.compile('[a-zA-Z]+:')
-        INIT_MEMORY = problem.init_memory.split()
+        INIT_MEMORY = []
+        for x in problem.init_memory.split():
+            if x not in LETTERS and x != '*':
+                x = int(x)
+            INIT_MEMORY.append(x)
         PARSED_CODE = []
         LABELS = {}
 
